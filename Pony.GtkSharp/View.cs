a88eba8ff3192a3fd5d87bb91c18184297ad7c96
Daniil Smirnov <danslapman@gmail.com>
2014-10-20 23:21:26 +0400

    Project skeleton

---
+++ Pony.GtkSharp/View.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Pony;
+
+namespace Pony.GtkSharp
+{
+	public class View<T> : Gtk.Window where T : class, new()
+	{
+		protected View (IPonyApplication ponyApplication) : base(Gtk.WindowType.Toplevel)
+		{
+		}
+	}
+}
+
